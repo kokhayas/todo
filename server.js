948cb0959fa727d117f1af2ed2424439a682734f
KokiHayashi <1234.884.koki@gmail.com>
2022-04-16 23:22:51 +0900

    modified

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 
 const cookieParser = require("cookie-parser");
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
 app.use(cookieParser());
 
@@ -34,24 +34,38 @@ app.get("/", async (request, response) => {
 app.post("/", async (request, response) => {
 	await client.todo.create({data: {name: request.body.newTodo}});
 
+	const count = parseInt(request.cookies.count) || 0;
+	const newCount = count + 1;
+	response.cookie("count", newCount.toString());
+
 	const todos = await client.todo.findMany();
 	const template = fs.readFileSync("template.ejs").toString();
-	const html = ejs.render(template, {todos: todos});
+	const html = ejs.render(template, {todos: todos, newCount: newCount});
 	response.send(html);
 });
 
-// app.get("/form", (request, response) => {
-// 	const html = fs.readFileSync("./form.html").toString();
-// 	response.send(html);
-// });
+app.get("/form", (request, response) => {
+	const html = fs.readFileSync("./form.html").toString();
+	response.send(html);
+});
 
-// app.post("/send", async (request, response) => {
-// 	await client.todo.create({data: {name: request.body.newTodo}});
-// 	const todos = await client.todo.findMany();
-// 	const template = fs.readFileSync("template.ejs").toString();
-// 	const html = ejs.render(template, {todos: todos});
-// 	response.send(html);
-// });
+app.post("/send", async (request, response) => {
+	await client.todo.create({data: {name: request.body.newTodo}});
+
+	// const todos = await client.todo.findMany();
+	// const template = fs.readFileSync("template.ejs").toString();
+	// const html = ejs.render(template, {todos: todos});
+	// response.send(html);
+	const html = fs.readFileSync("./sended.html").toString();
+	response.send(html);
+
+});
+
+app.get("/erase", async (request, response) => {
+	await client.todo.deleteMany();
+	const html = fs.readFileSync("./erase.html").toString();
+	response.send(html);
+})
 
 // app.listen(3000);
 
